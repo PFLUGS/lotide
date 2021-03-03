0d6e2cbe2d9526197d1e180552e64417d00bd43f
Sarah <sarah.pflug@shopify.com>
2021-03-03 01:50:31 +0000

    eqArrays lesson

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+
+let eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), false);
+
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual('hello', 'hello');
+// assertEqual(1, 2);
