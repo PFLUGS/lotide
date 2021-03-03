66141f3ab49ee3eafff9742faba68e9328d40e58
Sarah <sarah.pflug@shopify.com>
2021-03-03 14:11:11 +0000

    took out un necessary test block of code

--- without.js
+++ without.js
@@ -32,26 +32,3 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-
-
-
-
-
-
-
-
-
-
-
-// const without = function(source, itemsToRemove){
-//   let result = [];
-//   for (let i = 0; i < source.length; i++){
-//     if (itemsToRemove.indexOf(source[i]) == -1){
-//       result.push(source[i])
-//     }
-//   }
-//   console.log(result)
-//   return result
-// }
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
