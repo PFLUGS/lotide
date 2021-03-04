fe903ef1035f2c17bd7b1077dd423228ddb3cd7b
Sarah <sarah.pflug@shopify.com>
2021-03-04 04:01:36 +0000

    new eqArrays function added, the other broke

--- middle.js
+++ middle.js
@@ -1,12 +1,18 @@
 let eqArrays = function (array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  let accumulator = 0;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        accumulator++;
+      }
     }
   }
-  return true;
-};
-
+  if (accumulator === array1.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
