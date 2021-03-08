5190dfdaa212925f021ab83d2c016d3d6265ee06
Sarah <sarah.pflug@shopify.com>
2021-03-08 23:28:02 +0000

    refactor of tests and added module export

--- letterPositions.js
+++ letterPositions.js
@@ -1,26 +1,4 @@
-let eqArrays = function (array1, array2) {
-  let accumulator = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        accumulator++;
-      }
-    }
-  }
-  if (accumulator === array1.length) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const letterPositions = function (sentence) {
@@ -39,6 +17,7 @@ const letterPositions = function (sentence) {
   return results
 };
 
+module.exports = letterPositions
 console.log(letterPositions("lighthouse in the house"));
 
 assertArraysEqual(letterPositions("sarah").a, [1,3]);
