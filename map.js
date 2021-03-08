697ed9dcfa4e27100c1f325867818a56aa048bc2
Sarah <sarah.pflug@shopify.com>
2021-03-08 23:24:53 +0000

    refactored tests

--- map.js
+++ map.js
@@ -1,26 +1,5 @@
-let eqArrays = function (array1, array2) {
-  let accumulator = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        accumulator++;
-      }
-    }
-  }
-  if (accumulator === array1.length) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
 
 const words = ["ground", "control", "to", "major", "tom"];
 
