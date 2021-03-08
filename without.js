d4c097c900930b0f343501adab1539c021e8df89
Sarah <sarah.pflug@shopify.com>
2021-03-08 23:19:06 +0000

    refactored

--- without.js
+++ without.js
@@ -1,26 +1,5 @@
-let eqArrays = function (array1, array2) {
-  let accumulator = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        accumulator++;
-      }
-    }
-  }
-  if (accumulator === array1.length) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const without = function(source, itemsToRemove){
   let result = [];
@@ -39,3 +18,4 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without
