5ea6f1ae4ca0bf52e15c1e6e67e54d1a5e02372b
Sarah <sarah.pflug@shopify.com>
2021-03-08 21:23:11 +0000

    middle refactoring and middle test file added

--- middle.js
+++ middle.js
@@ -1,30 +1,7 @@
-let eqArrays = function (array1, array2) {
-  let accumulator = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        accumulator++;
-      }
-    }
-  }
-  if (accumulator === array1.length) {
-    return true;
-  } else {
-    return false;
-  }
-}
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
-
 let middle = function (array) {
   // find the middle number
   let middleIndex = Math.floor(array.length / 2)
-  let middleNumber = array[middleIndex]
+  let middleNumber = [array[middleIndex]]
   // if array is 0 , 1 or 2 numbers we need to output an empty array
   if (array.length <= 2) {
     return [];
@@ -38,8 +15,5 @@ let middle = function (array) {
 
 };
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([]), []);
+module.exports = middle;
+
